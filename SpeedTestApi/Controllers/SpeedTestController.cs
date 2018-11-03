c6ce26ce2865fdfb71a4ed0b75091a35cba5c887
Simen Samuelsen <sisa@cxmac-00429.local>
2018-11-03 11:56:08 +0100

    First api

---
+++ SpeedTestApi/Controllers/SpeedTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpeedTestApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SpeedTestController : ControllerBase
+    {
+        [Route("ping")]
+        [HttpGet]
+        public ActionResult<string> Ping()
+        {
+            return Ok("PON  G");
+        }
+    }
+}
